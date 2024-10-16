cb6a2af0608368bb6db940d85742ca99a359c692
Aryaman Pathak <aryamansp13@gmail.com>
2024-10-16 15:41:54 +0530

    Add files via upload

---
+++ cpp exp18c.cpp
@@ -0,0 +1,17 @@
+//Aryaman Pathak EnTCA2 031
+//stack imp using stl
+#include <iostream>
+#include <stack>
+using namespace std;
+
+int main() {
+    stack<int>st;
+st.push(30);
+st.push(20);
+st.push(10);
+
+    
+cout<< "Top element of the stack: " <<st.top() <<endl;
+
+    return 0;
+}
